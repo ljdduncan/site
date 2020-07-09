305add497eb0560b6338205ccc9d7881c55cc55f
ljdduncan <ljdduncan@gmail.com>
2020-07-08 22:32:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {
+      puzzleBoxes: new Array(8).fill().map((item, idx) => Math.random() >= 0.5),
+      text: ''
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCBChange = this.handleCBChange.bind(this);
+  }
+  renderCB(i) {
+    return (<input
+              type="checkbox"
+              name= ""
+              checked={this.state.puzzleBoxes[i]}
+              onChange={()=>this.handleCBChange(i)}
+            />);
   }
   render() {
     return (
@@ -27,6 +40,27 @@ class App extends React.Component {
                 Search
               </button>
             </form>
+            <form className="App-border">
+              <label>
+                8 Switch Puzzle
+              </label>
+              <div className="board-row">
+                {this.renderCB(0)}
+                {this.renderCB(1)}
+                {this.renderCB(2)}
+              </div>
+              <div className="board-row">
+                {this.renderCB(7)}
+                {<span>&nbsp;&nbsp;&nbsp;</span>}
+                {this.renderCB(3)}
+              </div>
+              <div className="board-row">
+                {this.renderCB(6)}
+                {this.renderCB(5)}
+                {this.renderCB(4)}
+              </div>
+              
+            </form>
         </header>
       </div>
     );
@@ -38,6 +72,22 @@ class App extends React.Component {
     e.preventDefault();
     window.open('https://www.google.com/search?q=' + this.state.text)
     this.setState({text: ""});
+  }
+  handleCBChange(e) {
+    const puzzleBoxes = this.state.puzzleBoxes.slice();
+    puzzleBoxes[e] = !puzzleBoxes[e];
+    if (e == 0) {
+      puzzleBoxes[7] = !puzzleBoxes[7];
+    } else {
+      puzzleBoxes[e-1] = !puzzleBoxes[e-1];
+    }
+    if (e == 7) {
+      puzzleBoxes[0] = !puzzleBoxes[0];
+    } else {
+      puzzleBoxes[e+1] = !puzzleBoxes[e+1];
+    }
+    this.setState({puzzleBoxes: puzzleBoxes});
+    
   }
 }
 
