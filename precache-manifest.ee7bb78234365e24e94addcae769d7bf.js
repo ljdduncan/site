8851e450413a21fd9b99743d21f72e913ebd5480
ljdduncan <ljdduncan@gmail.com>
2020-07-08 21:15:49 -0400

    Updates

---
+++ precache-manifest.ee7bb78234365e24e94addcae769d7bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97af1c15ab4d4225459986c731a2b588",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "7325522e5fcd1c779f4e",
+    "url": "/site/static/css/main.19041792.chunk.css"
+  },
+  {
+    "revision": "384aa0a84cb76ae831e3",
+    "url": "/site/static/js/2.266f075c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/site/static/js/2.266f075c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7325522e5fcd1c779f4e",
+    "url": "/site/static/js/main.a0799f9c.chunk.js"
+  },
+  {
+    "revision": "a6f832edb43cb75d95e7",
+    "url": "/site/static/js/runtime-main.6815615a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
